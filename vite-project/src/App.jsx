bdec90b70b4caa421c39727a89f390144989ba34
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-10-24 14:24:51 +0200

    adding setup files

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div  className="bg-primary w-full overflow-hidden" >
+       <h1>hello world</h1>
+     </div>
+  )
+}
+
+export default App
